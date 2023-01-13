6725e40b2aba5896c8b0eef3e21a0d0b53577c05
Jonah Bard <jonahbard@gmail.com>
2023-01-13 16:05:06 +0200

    Initial Commit

---
+++ Quizzard/QuizzardApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzardApp.swift
+//  Quizzard
+//
+//  Created by Jonah Bard on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizzardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
